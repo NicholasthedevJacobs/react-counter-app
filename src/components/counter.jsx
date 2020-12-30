ac005922ad7836bf0131b69261164f0d41e2ff41
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-12-30 17:53:37 -0600

    Learned how to add styles to components within a class, in a similar way that I access state within the class.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,12 +5,17 @@ class Counter extends Component {
         count: 0, 
     };
 
+    styles = {
+        fontSize: '10px',
+        fontWeight: 'bold'
+    };
+
     render(){
         return (
-        <React.Fragment>
-            <span className= 'badge badge-primary m-2'>{this.formatCount()}</span>
+        <div>
+            <span style={ this.styles } className= 'badge badge-primary m-2'>{this.formatCount()}</span>
             <button className= 'btn btn-secondary btn-sm'>Increment</button>
-        </React.Fragment>
+        </div>
         );
     }
 
